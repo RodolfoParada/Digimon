3f9649213c3b2d6e2f9f68db3a13e5c085d0ca16
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-04-18 01:13:19 -0400

    monito

--- asset/javascript/otra.js
+++ asset/javascript/otra.js
@@ -6,25 +6,31 @@ const url = `https://digimon-api.vercel.app/api/digimon/`
 fetch(url)
     .then(response => response.json())
     .then(data => {
+        console.log(data)
+        const img = document.querySelector('img')
+        img.src = data[0].img
 
-        for (const key in data) {
-            console.log(key);
-            console.log(data[key])
-            console.log(data[key].name)
-            console.log(data[key].img)
-            console.log(data[key].level)
+        // const h5 = document.querySelector('h5')
+        // h5 = data[0].name
+
+        // for (const key in data) {
+        //     console.log(key);
+        //     console.log(data[key])
+        //     console.log(data[key].name)
+        //     console.log(data[key].img)
+        //     console.log(data[key].level)
 
 
-            $("#card").append(`
-             
-                 <h5>${data[key].name}</h5>
-                 <img>${data[key].img}</img>
-                 <card-text">${data[key].level}</p>
-             
-            
-            
-            `)
-        }
+        //     $("#card").append(`
+
+        //          <h5>${data[key].name}</h5>
+        //          <img>${data[key].img}</img>
+        //          <card-text">${data[key].level}</p>
+
+
+
+        //     `)
+        // }
 
 
     })
