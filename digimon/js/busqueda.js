90b970899dc4836f2e4459d5cda4377a5ced806c
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-04-29 21:57:37 -0400

    mejoras

--- digimon/js/busqueda.js
+++ digimon/js/busqueda.js
@@ -1,21 +1,21 @@
 const checkNivel = document.getElementById("checkNivel");
 $(document).ready(function () {
 
-            $("#botonBuscar").on("click", function () {
-                if (!checkNivel.checked) {
-                    if ($("#digimonName").val() === "") {
-                        alert("Debe ingresar un nombre");
-                        return
-                    }
-                    $.ajax({
-                        url: "https://digimon-api.vercel.app/api/digimon/name/" + $("#digimonName").val(),
-                        type: "GET",
-                        dataType: "json",
-                        success: function (data) {
-                            $("#digimon").removeClass("d-none");
-                            $("#digimons").addClass("d-none");
-                            let html = "";
-                            html += `
+    $("#botonBuscar").on("click", function () {
+        if (!checkNivel.checked) {
+            if ($("#digimonName").val() === "") {
+                alert("Debe ingresar un nombre");
+                return
+            }
+            $.ajax({
+                url: "https://digimon-api.vercel.app/api/digimon/name/" + $("#digimonName").val(),
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    $("#digimon").removeClass("d-none");
+                    $("#digimons").addClass("d-none");
+                    let html = "";
+                    html += `
                     <div class="col-12 col-md-4 col-lg-3 col-xl-2 text-center">
                     <div class="card mt-4">
                         <div class="card-header">
@@ -28,23 +28,23 @@ $(document).ready(function () {
                     </div>
                 </div>
              `;
-                            $("#digimon").html(html);
-                        },
-                        error: function () {
-                            alert("Error al buscar el digimon");
-                        }
-                    });
-                } else {
-                    $.ajax({
-                        url: "https://digimon-api.vercel.app/api/digimon/level/" + $("#selectNivel").val(),
-                        type: "GET",
-                        dataType: "json",
-                        success: function (data) {
-                            $("#digimon").addClass("d-none");
-                            $("#digimons").removeClass("d-none");
-                            let html = "";
-                            data.forEach(digimon => {
-                                html += `
+                    $("#digimon").html(html);
+                },
+                error: function () {
+                    alert("Error al buscar el digimon");
+                }
+            });
+        } else {
+            $.ajax({
+                url: "https://digimon-api.vercel.app/api/digimon/level/" + $("#selectNivel").val(),
+                type: "GET",
+                dataType: "json",
+                success: function (data) {
+                    $("#digimon").addClass("d-none");
+                    $("#digimons").removeClass("d-none");
+                    let html = "";
+                    data.forEach(digimon => {
+                        html += `
                         <div class="col-12 col-md-4 col-lg-3 col-xl-2 text-center">
                             <div class="card mt-4">
                                 <div class="card-header">
@@ -57,25 +57,26 @@ $(document).ready(function () {
                             </div>
                         </div>
                         `;
-                            });
-                            $("#digimons").html(html);
-                        },
-                        error: function () {
-                            if ($("#selectNivel").val() === "0") {
-                                alert("Debe seleccionar un nivel");
-                            } else {
-                                alert("Error al buscar los digimon de ese nivel");
-                            }
-                        }
                     });
+                    $("#digimons").html(html);
+                },
+                error: function () {
+                    if ($("#selectNivel").val() === "0") {
+                        alert("Debe seleccionar un nivel");
+                    } else {
+                        alert("Error al buscar los digimon de ese nivel");
+                    }
                 }
             });
-            $("#checkNivel").on("change", function () {
-                if ($(this).prop("checked")) {
-                    $("#digimonName").addClass("d-none");
-                    $("#selectNivel").removeClass("d-none");
-                } else {
-                    $("#digimonName").removeClass("d-none");
-                    $("#selectNivel").addClass("d-none");
-                }
-            });
+        }
+    });
+    $("#checkNivel").on("change", function () {
+        if ($(this).prop("checked")) {
+            $("#digimonName").addClass("d-none");
+            $("#selectNivel").removeClass("d-none");
+        } else {
+            $("#digimonName").removeClass("d-none");
+            $("#selectNivel").addClass("d-none");
+        }
+    });
+});
